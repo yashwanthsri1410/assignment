d140fd5037eef583186d15d41f861010e6806c31
yashwanthsri1410 <yashwanthsri1410@gmail.com>
2022-03-22 15:44:01 +0530

    'DAY2'

---
+++ DAY2/appendToString.js
@@ -0,0 +1,8 @@
+// add whatever parameters you deem necessary - good luck!
+function appendToString(arg1, arg2) {
+    return arg1 + arg2
+}
+
+let output = appendToString("", "test");
+
+console.log(output)
